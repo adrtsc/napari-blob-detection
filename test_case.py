d3d7fd6231f7d3283f416bdd6b217a16d599f907
adrtsc <adrian.tschan@uzh.ch>
2021-05-21 23:32:01 +0200

    clean-up

--- test_case.py
+++ test_case.py
@@ -5,9 +5,10 @@ Created on Thu May 13 06:13:09 2021
 @author: Adria
 """
 %gui qt5
-import napari_blob_detection
-from skimage import data
 import napari
+import napari_blob_detection
+
+from skimage import data
 from skimage.color import rgb2gray
 
 image = data.hubble_deep_field()[0:500, 0:500]
@@ -19,7 +20,3 @@ viewer.add_image(image_gray, name='img')
 # In the viewer window navigate to:
 # "Plugins" -> "Add Dock Widget" -> "napari-blob-detection" -> "detect_blobs"
 
-viewer.window.add_dock_widget(blob_detection())
-
-filter_widget = filter_widget()
-viewer.window.add_dock_widget(filter_widget)
