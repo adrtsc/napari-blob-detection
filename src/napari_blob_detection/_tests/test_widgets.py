06f8b381d902d46cbcf4d5ed3ef4a115101ba560
adrtsc <adrian.tschan@uzh.ch>
2022-04-02 13:19:11 +0200

    update tests

--- src/napari_blob_detection/_tests/test_widgets.py
+++ src/napari_blob_detection/_tests/test_widgets.py
@@ -8,14 +8,13 @@ from skimage.color import rgb2gray
 def test_mock():
     pass
 
-'''
 
 def test_blob_docking(make_napari_viewer):
     viewer = make_napari_viewer()
     viewer.window.add_dock_widget(blob_detection())
     assert "Dock widget 1" in viewer.window._dock_widgets,\
         'blob detection has not been docked'
-
+'''
 
 def test_filter_docking(make_napari_viewer):
     viewer = make_napari_viewer()
